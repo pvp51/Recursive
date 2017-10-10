b8d62dceadfb8866b33dcda41e6ee14727f0863f
Parth <parth5494@live.com>
2017-10-09 22:50:54 -0400

    main class

--- src/assignment2/BuildTree.java
+++ src/assignment2/BuildTree.java
@@ -1,9 +1,27 @@
 package assignment2;
 
+import java.util.Scanner;
+
 public class BuildTree {
 
+    private class TreeType {
+        private int data;
+        private TreeType next;
+        private TreeType left;
+        private TreeType right;
+    }
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String input;
+		String output;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter the input expression: ");
+		input = scan.nextLine();
+		System.out.println("The input expression: "+input);
+		
 
 	}
 
